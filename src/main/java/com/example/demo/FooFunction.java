22e01aab09641ce04979eeed388b9170968962c4
Dave Syer <dsyer@vmware.com>
2021-05-18 12:16:09 +0100

    Update for Spring Cloud Function snapshots
    There have been some bug fixes in Spring CLoud Function. Snapshots
    are better until 3.1.3 is out.

--- src/main/java/com/example/demo/FooFunction.java
+++ src/main/java/com/example/demo/FooFunction.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import java.util.function.Function;
+
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.stereotype.Component;
 
 @Component
+@TypeHint(types = Foo.class)
 public class FooFunction implements Function<Foo, Foo> {
 
   @Override
